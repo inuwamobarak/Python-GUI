171b533ee1af36b89d01ef158268f4310110154f
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-04-16 13:05:06 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+name = input("Enter your name:")
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi(name)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
